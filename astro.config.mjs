7b8baa09aff28b18e774fbf88e7bbb34cf029f0c
whustafree <magustoregustavo@gmail.com>
2026-02-22 21:22:22 +0000

    sw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
 export default defineConfig({
   integrations: [tailwind()],
-
-  // <-- ESTA ES LA LÍNEA MÁGICA QUE FALTABA
-  output: 'server',
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output: 'server'
 });
