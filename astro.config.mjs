bb950f8b5c6dcd5d66fc273f12eefab8803d3fb9
whustafree <magustoregustavo@gmail.com>
2026-02-22 22:25:27 +0000

    Instalar adaptador de Vercel para SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  adapter: vercel()
 });
